336e819788fc33d22e2224f8126e856102f4c594
Stefanie Tellex <stefie10@alum.mit.edu>
2013-10-22 17:11:30 -0400

    recording stuff

--- src/kinrecord.py
+++ src/kinrecord.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
+import argparse
+import rospy
+import cv
+from PIL import Image
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+import sensor_msgs
+from cv_bridge import CvBridge, CvBridgeError
+
+class Kinrecord(object):
+    def __init__(self):
+        self.bridge = CvBridge()
+        
+    def callback(self, image_msg):
+        cv_image = self.bridge.imgmsg_to_cv(image_msg, "bgr8")
+        print cv_image
+        cv.ShowImage("Image window", cv_image)
+        cv.WaitKey(3)
+  
 
 def main():
-    print "hello world"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bag-filename", dest="bag_fname")
+
+    kinrecord = Kinrecord()
+    rospy.init_node('kinrecord')
+    rospy.Subscriber("/camera/rgb/image_raw", sensor_msgs.msg.Image, 
+                     kinrecord.callback)
+    rospy.spin()
 
 if __name__ == "__main__":
     main()
