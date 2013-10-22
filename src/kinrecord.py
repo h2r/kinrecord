0fc0346eb0d42bbe797ca50a1ee58e63e6a8d06b
Stefanie Tellex <stefie10@alum.mit.edu>
2013-10-22 17:20:09 -0400

    record and save a jpg

--- src/kinrecord.py
+++ src/kinrecord.py
@@ -2,22 +2,27 @@
 import argparse
 import rospy
 import cv
+import cv2
 from PIL import Image
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
 import sensor_msgs
 from cv_bridge import CvBridge, CvBridgeError
-
+import time
 class Kinrecord(object):
     def __init__(self):
         self.bridge = CvBridge()
         
     def callback(self, image_msg):
+        rospy_time = image_msg.header.stamp
+        python_time = time.ctime(rospy_time.to_sec())
+        #print python_time
+
         cv_image = self.bridge.imgmsg_to_cv(image_msg, "bgr8")
-        print cv_image
-        cv.ShowImage("Image window", cv_image)
-        cv.WaitKey(3)
+        cv.SaveImage("test.jpg", cv_image)
+        #cv.ShowImage("Image window", cv_image)
+        #cv.WaitKey(3)
   
 
 def main():
